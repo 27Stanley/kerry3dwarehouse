a3f66805497efee15be8ac6dd3adeece6a18c9a8
Stanley <27.stanleylch@gmail.com>
2024-07-02 17:09:25 +0100

    header component from mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ThemeProvider } from "@mui/material";
+import Theme from "./themes/Theme";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/themes/Theme.tsx
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#343E3D", // primary color
+    },
+    secondary: {
+      main: "#323031", // secondary color
+    },
+    info: {
+      main: "#9695BF", // info color
+    },
+    success: {
+      main: "#C5DED6", // success color
+    },
+    warning: {
+      main: "#C9FFCE", // warning color
+    },
+    background: {
+      default: "#C5DED6", // background color
+    },
+    text: {
+      primary: "#343E3D", // text color
+    },
+  },
+});
+
+export default theme;
