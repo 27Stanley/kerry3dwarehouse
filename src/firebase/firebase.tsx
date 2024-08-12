081099c9b936bb8c9fa29e289c3f4f9a53395e5f
Stanley <stanleyleung927@gmail.com>
2024-08-12 23:10:08 +0100

    firebase .env

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjv8JbwXifdtAlthCUhF86eAI3-eoX0pI",
-  authDomain: "kerry3dwarehouse.firebaseapp.com",
-  projectId: "kerry3dwarehouse",
-  storageBucket: "kerry3dwarehouse.appspot.com",
-  messagingSenderId: "739727954574",
-  appId: "1:739727954574:web:f8cbb0936ad5553c042fdf",
-  measurementId: "G-8P3MWCMKMY",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
